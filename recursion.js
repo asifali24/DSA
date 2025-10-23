30614b6564c6b11f6c146b4103d0604519984037
asifali24 <aliasifali231@gmail.com>
2025-10-23 22:16:00 +0530

    sum in array

--- recursion.js
+++ recursion.js
@@ -5,4 +5,15 @@ const recSumNNumber = (n) => {
     return n + recSumNNumber(n-1)
 }
 
-console.log(recSumNNumber(5),"======")
+console.log(recSumNNumber(5), "======")
+
+//sum of all the no in array via recurssion
+
+let arr = [2, 4, 5, 7, 8]
+
+const arraySum = (a) => {
+    if (!a.length) return 0
+    return a.pop() + arraySum(a)
+}
+
+console.log("====",arraySum(arr))
